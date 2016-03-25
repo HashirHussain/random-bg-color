b6b7546d317fcd2a03cb95cf9c42a3e504b0743a
Virendra Chouhan <chouhan.virendra@gmail.com>
2016-03-25 13:00:13 +0530

    Fix #2

--- random-bg-color.js
+++ random-bg-color.js
@@ -7,22 +7,16 @@
 (function(win, doc) {
     'use strict';
 
-    function init(colors, targetElements) {
-        this.colors = colors || [];
-        this.targetElements = targetElements || '';
-    }
-
+    function init() {}
     init.prototype.colors = function(colors) {
-        return this.colors = colors;
+        this.colors = colors;
     };
-
     init.prototype.targetElements = function(targetElements) {
-        return this.targetElements = targetElements;
-    }
+        this.targetElements = targetElements;
+    };
     init.prototype.apply = function() {
         var color = this.colors[Math.floor(Math.random() * this.colors.length)];
-        doc.getElementById(this.targetElements).style.backgroundColor = color;
+        doc.getElementById(this.targetElements).style.background = color;
     };
-
     win.bgColors = (typeof(win.bgColors) === "undefined") ? new init() : 'bgColors is already defined';
 })(window, document);
